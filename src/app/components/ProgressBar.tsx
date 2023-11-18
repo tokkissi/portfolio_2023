00f56c8dd65d39ee469c29dd62a53a87fda0d18e
tokkissi <alal4674@gmail.com>
2023-11-18 23:58:13 +0900

    feat: add IntersectionObserver to ProgressBar

--- src/app/components/ProgressBar.tsx
+++ src/app/components/ProgressBar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import styles from "./ProgressBar.module.css";
 
 type ProgressBarProps = {
@@ -15,63 +15,82 @@ export default function ProgressBar({
   const [counter, setCounter] = useState(0);
   const fullDasharray = 2 * Math.PI * 35; // 반지름 70으로 원 둘레 구하기
   const [dashoffset, setDashoffset] = useState(fullDasharray); // counter 값에 따라 dashoffset을 계산하여, 원이 채워지는 비율을 counter의 값과 동기화하여 업데이트함
+  const progressBarRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    // 각 percentage 증가마다 걸리는 시간을 계산
     const intervalDuration = animationDuration / percentage;
     const incrementPerInterval =
-      percentage / (animationDuration / intervalDuration); // 각 interval에서 증가시킬 counter 값
+      percentage / (animationDuration / intervalDuration);
 
-    const interval = setInterval(() => {
-      setCounter((prevCounter) => {
-        const nextCounter = prevCounter + incrementPerInterval;
-        // counter 상태와 동기화하여 stroke-dashoffset도 업데이트
-        setDashoffset(fullDasharray - fullDasharray * (nextCounter / 100));
+    let progressBarElement = progressBarRef.current; // ref에서 요소를 변수에 복사
 
-        if (nextCounter >= percentage) {
-          clearInterval(interval);
-          return percentage;
+    const intersectionObserver = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting) {
+          const interval = setInterval(() => {
+            setCounter((prevCounter) => {
+              const nextCounter = prevCounter + incrementPerInterval;
+              setDashoffset(
+                fullDasharray - fullDasharray * (nextCounter / 100)
+              );
+
+              if (nextCounter >= percentage) {
+                clearInterval(interval);
+                return percentage;
+              }
+              return nextCounter;
+            });
+          }, intervalDuration);
+
+          return () => clearInterval(interval);
         }
-        return nextCounter;
-      });
-    }, intervalDuration);
+      },
+      { threshold: 0.85 }
+    );
 
-    return () => clearInterval(interval);
+    if (progressBarElement) {
+      intersectionObserver.observe(progressBarElement);
+    }
+
+    return () => {
+      if (progressBarElement) {
+        intersectionObserver.unobserve(progressBarElement);
+      }
+      intersectionObserver.disconnect();
+    };
   }, [percentage, fullDasharray, animationDuration]);
 
   return (
     <>
-      <div>
-        <div className={styles.skill}>
-          <div className={styles.outer}>
-            <div className={styles.inner}>
-              <div className={styles.number}>{Math.round(counter)}%</div>
-            </div>
+      <div ref={progressBarRef} className={styles.skill}>
+        <div className={styles.outer}>
+          <div className={styles.inner}>
+            <div className={styles.number}>{Math.round(counter)}%</div>
           </div>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            version="1.1"
-            width="80px"
-            height="80px"
-            className={styles.localSvg}
-          >
-            <defs>
-              <linearGradient id="GradientColor">
-                <stop offset="0%" stopColor="#e91e63" />
-                <stop offset="100%" stopColor="#673ab7" />
-              </linearGradient>
-            </defs>
-            <circle
-              className={styles.localCircle}
-              cx="40"
-              cy="40"
-              r="35"
-              strokeLinecap="round"
-              strokeDasharray={fullDasharray}
-              strokeDashoffset={dashoffset}
-            />
-          </svg>
         </div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          version="1.1"
+          width="80px"
+          height="80px"
+          className={styles.localSvg}
+        >
+          <defs>
+            <linearGradient id="GradientColor">
+              <stop offset="0%" stopColor="#e91e63" />
+              <stop offset="100%" stopColor="#673ab7" />
+            </linearGradient>
+          </defs>
+          <circle
+            className={styles.localCircle}
+            cx="40"
+            cy="40"
+            r="35"
+            strokeLinecap="round"
+            strokeDasharray={fullDasharray}
+            strokeDashoffset={dashoffset}
+          />
+        </svg>
       </div>
     </>
   );
